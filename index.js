64752389969a7bb05f493859a6b80db40e5fb8f2
kunitoshi291 <takevadogu@outlook.jp>
2021-11-22 18:19:05 +0900

    タスク追加機能と削除機能を追加

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-import {App} from "./src/App.js";
-const app = new App();
+const taskValue = document.getElementsByClassName('task_value')[0];
+const taskSUbmit = document.getElementsByClassName('task_submit')[0];
+const taskList = document.getElementsByClassName('task_list')[0];
+
+// 追加ボタンを作成
+const addTasks = (task)=> {
+    // 入力したタスクを追加・表示
+    const listItem = document.createElement('li');
+    const showItem = document.appendChild(listItem);
+    showItem.innerHTML = task;
+
+    // タスクに削除ボタンを付与
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'DELETE';
+    listItem.appendChild(deleteButton);
+
+    // 削除ボタンをクリックし、イベントが発生
+    deleteButton.addEventListener('click', evt => {
+        evt.preventDefault();
+        deleteTasks(deleteButton);
+    });
+};
+
+// 削除ボタンにタスクを消す機能を付与
+const deleteTasks = ()=> {
+    const chosenTask = deleteButton.closet('li');
+    taskList.removeChild(chosenTask);
+};
+
+// 追加ボタンをクリックし、イベントが発生
+taskSubmit.addEventListener('click', evt =>{
+    evt.preventDefault();
+    const task = taskValue.value;
+    addTasks(task);
+    taskValue.value = '';
+});
+
+
+
+
+
+
+
+
+
 
